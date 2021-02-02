7d344084ec22d189ce663a64a6b41fe59626041c
olivierwilkinson <olivier.wilkinson@gmail.com>
2021-02-02 12:17:13 +0000

    Make modifyFragment and modifyQuery experimental

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
+/* eslint-disable babel/camelcase */
+
 import combine from './combine'
 import evict from './evict'
 import modify from './modify'
-import modifyFragment from './modifyFragment'
-import modifyQuery from './modifyQuery'
 import writeFragment from './writeFragment'
 import writeQuery from './writeQuery'
+import experimental_modifyFragment from './modifyFragment'
+import experimental_modifyQuery from './modifyQuery'
 
 export {
   combine,
   evict,
   modify,
-  modifyFragment,
-  modifyQuery,
   writeFragment,
   writeQuery,
+  experimental_modifyFragment,
+  experimental_modifyQuery,
 }
 
 export * from './types'
